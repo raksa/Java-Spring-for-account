1d4d509dd97871a6bd8d03573ee2a597bfa4347c
raksa <eng.raksa@gmail.com>
2017-04-07 16:11:13 +0700

    add files

---
+++ src/test/java/com/hellokoding/account/AppTest.java
@@ -0,0 +1,13 @@
+package com.hellokoding.account;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AppTest {
+	
+	@Test
+	public void multiplicationOfZeroIntegersShouldReturnZero(){
+		Assert.assertEquals("10 x 0 must be 0", 0, 10 * 1);
+	}
+}
