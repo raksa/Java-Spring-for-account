7780a5dfe8700b765dd7b630f732191cc9f6375d
raksa <eng.raksa@gmail.com>
2017-05-29 16:10:20 +0700

    update dummy db

--- databases/load_data.sql
+++ databases/load_data.sql
@@ -8,6 +8,7 @@ INSERT INTO `user_account` VALUES (1,'username', 'test@mail.local','$2a$11$I186o
 INSERT INTO `user_account` VALUES (2,'username1', 'test1@mail.local','$2a$11$I186oy5zBV1pMSA3wQo6iO0RhYKNkzG7XB.n1hCVKq7q6MzOlM29C');
 
 INSERT INTO `user_role` VALUES (1,1);
+INSERT INTO `user_role` VALUES (2,1);
 
 -- 
 -- Set auto_increment
